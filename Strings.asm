bcd3a1b0e742bbef47c0bd5c4e20d8fad7b2cd94
Franklin0770 <fcrocetta073@gmail.com>
2026-05-01 20:04:45 +0200

    Forgot to stage the actual changes

--- Strings.asm
+++ Strings.asm
@@ -1,3 +1,6 @@
+	align 2
+	save
+	listing off
 	charset "Assets/character_mapping.bin"
 
 DebugText:
@@ -11,4 +14,25 @@ DebugText:
 	dfntxt "Press B to play a note continuously", 0, 8
 	dfntxt "Press C to restart the music", 0, 9
 
-	charset
+	restore
+
+ValueInformation:
+	dc.l sampleIndex					; Variable address
+	dc.l vdpCoordinates(15,0)			; VDP coordinates
+	dc.w PrintLong-PrintValues.Base-2	; Jump offset
+
+	dc.l z80Samples
+	dc.l vdpCoordinates(24,1)
+	dc.w PrintWord-PrintValues.Base-2
+
+	dc.l lastSample
+	dc.l vdpCoordinates(18,2)
+	dc.w PrintByte-PrintValues.Base-2
+
+	dc.l randomByte
+	dc.l vdpCoordinates(23,3)
+	dc.w PrintByte-PrintValues.Base-2
+
+	dc.l frameCount
+	dc.l vdpCoordinates(15,4)
+	dc.w PrintLong-PrintValues.Base-2
